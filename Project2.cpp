31e7b4a2c9d526c8ef63a19141a3c0ddfb38608a
Gleb Efimov <gleb.efimov@cdpr>
2021-02-20 11:43:08 +0300

    Add project files.

---
+++ Project2.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string a = "Hello World!";
+    std::cout << a << "\n";
+    std::cout << a.length() << "\n";
+    std::cout << "H !";
+}
